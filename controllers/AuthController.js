ec7420b6672343202f2926ed2e62b2bad0c57089
andito28 <andito763@gmail.com>
2024-04-06 10:55:11 +0800

    create feature login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import emailExits from "../libraries/emailExist.js";
+import bcrypt from "bcrypt";
 
 class AuthController {
   async register(req, res) {
@@ -13,14 +14,23 @@ class AuthController {
       if (!req.body.password) {
         throw { code: 428, message: "PASSWORD_IS_REQUIRED" };
       }
-
+      if (req.body.password.length < 6) {
+        throw { code: 428, message: "PASSWORD_MINIMUN_6_CHARACTER" };
+      }
       const isEmailExist = await emailExits(req.body.email);
 
       if (isEmailExist) {
         throw { code: 409, message: "EMAIL_ALREADY_EXIST" };
       }
 
-      const user = await User.create(req.body);
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(req.body.password, salt);
+
+      const user = await User.create({
+        fullname: req.body.fullname,
+        email: req.body.email,
+        password: hash,
+      });
 
       if (!user) {
         throw { code: 500, message: "USER_REGISTER_FAILED" };
@@ -29,6 +39,39 @@ class AuthController {
         status: true,
         message: "USER_REGISTER_SUCCES",
         user,
+      });
+    } catch (error) {
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
+
+  async login(req, res) {
+    try {
+      if (!req.body.email) {
+        throw { code: 428, message: "EMAIL_IS_REQUIRED" };
+      }
+      if (!req.body.password) {
+        throw { code: 428, message: "PASSWORD_IS_REQUIRED" };
+      }
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        throw { code: 400, message: "USER_NOT_FOUND" };
+      }
+
+      const isPasswordValid = await bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+      if (!isPasswordValid) {
+        throw { code: 400, message: "INVALID_PASSWORD" };
+      }
+      return res.status(200).json({
+        status: true,
+        message: "USER_LOGIN_SUCCESS",
+        fullname: user.fullname,
       });
     } catch (error) {
       return res.status(error.code || 500).json({
